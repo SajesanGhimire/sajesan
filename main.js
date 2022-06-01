103053c00e6236511ffded6349d4deb6a9fc9e18
SajesanGhimire <90140989+SajesanGhimire@users.noreply.github.com>
2022-06-01 07:36:38 +0545

    Update main.js

--- main.js
+++ main.js
@@ -60,26 +60,7 @@ ScrollReveal({
 ScrollReveal().reveal('.skills h3, .home-container .home-img, .about h3, .about h4', { delay: 100, origin: 'top'}); 
 // ScrollReveal().reveal('.home-container .home-img, .about h3, .about h4', { delay: 100, origin: 'right'}); 
 ScrollReveal().reveal('.home-info .home-btns, .skills .languages, .home .home-info h2,  .home-info p, .about .video, .projects .projects-container .project-card, .footer-container .info-follow h4', { delay: 100, origin: 'bottom'}); 
-// ScrollReveal().reveal('.home .home-info h2,  .home-info p, .about .video, .projects .projects-container .project-card, .footer-container .info-follow h4', { delay: 100, origin: 'left'} // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
-    });
-});
+// ScrollReveal().reveal('.home .home-info h2,  .home-info p, .about .video, .projects .projects-container .project-card, .footer-container .info-follow h4', { delay: 100, origin: 'left'} // owl carousel script });
+
+
+
